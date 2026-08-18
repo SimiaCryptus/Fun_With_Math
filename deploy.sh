753e68e28a531de1fd573a2cca4c56cfdaefdb9e
Andrew Charneski <acharneski@gmail.com>
2026-08-17 20:51:05 -0400

    prime generation experiment

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ aws s3 cp --recursive ./ s3://math.cognotik.com/ \
   --exclude "test/*" \
   --exclude ".pnpm/*" \
   --exclude "public/*" \
+  --exclude ".lake/*" \
   --exclude "videos/*" \
   --exclude "docs/*" \
   --exclude "scripts/*" \
