4db8e2c7190f1348b335059b7a9bc02752bdf7be
Andrew Charneski <acharneski@gmail.com>
2026-09-03 09:55:19 -0400

    sie and mosaic

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,27 +7,7 @@ npm run validate
 npm run manifest:build
 npm run build:seo:prod
 
-# TODO: convert to a smarter recursive copy in js that can ignore patterns recursively and skip noop uploads
-aws s3 cp --recursive ./ s3://math.cognotik.com/ \
-  --exclude "node_modules/*" \
-  --exclude "test/*" \
-  --exclude ".pnpm/*" \
-  --exclude "public/*" \
-  --exclude ".lake/*" \
-  --exclude "videos/*" \
-  --exclude "docs/*" \
-  --exclude "scripts/*" \
-  --exclude "package.json" \
-  --exclude "package-lock.json" \
-  --exclude "tsconfig.json" \
-  --exclude "webpack.config.js" \
-  --exclude ".git/*" \
-  --exclude "android-twa/*" \
-  --exclude "terraform/*" \
-  --exclude "demo/*" \
-  --exclude ".*/*" \
-  --exclude "*.sh" \
-  --exclude "LICENSE"
+node scripts/s3-sync.cjs --prune
 
 aws cloudfront create-invalidation \
   --distribution-id E2V4URD2KDRZ6N \
