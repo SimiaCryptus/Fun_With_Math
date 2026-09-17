50f95660a1e40c663c75b9f75ab345158ab09da4
Andrew Charneski <acharneski@gmail.com>
2026-09-17 11:33:51 -0400

    chore(split): extract experiments/magnet-lattice into Simiacryptus/magnet-lattice
    History preserved via git filter-repo. Re-added as a submodule in the next commit.

--- experiments/magnet-lattice/test/run.js
+++
@@ -1,8 +0,0 @@
-// Node test entrypoint: `node test/run.js`
-import './physics.test.js';
-import './integrator.test.js';
-import './analysis.test.js';
-import './lattice.test.js';
-import {summary} from './harness.js';
-
-summary();
